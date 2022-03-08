3d2852173f1ff57bb642d1810a852c22e7ea1968
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-03-08 20:32:33 +0800

    update

--- data_io/brats.py
+++ data_io/brats.py
@@ -54,11 +54,11 @@ class BraTS2019(BASE_DATASET):
 class BraTS2021(BraTS2019):
     def __init__(self, root, modalities=["t1", "t2"], learn_mode='train', extract_slice=[29, 100], noise_type='normal',
                  transform_data=None, client_weights=[1.0], data_mode='mixed', data_num=6000, data_paired_weight=0.2,
-                 data_moda_ratio=0.5, data_moda_case='case1', assigned_data=False, assigned_images=None):
+                 data_moda_ratio=0.5, data_moda_case='case1', assigned_data=False, assigned_images=None, seed=3):
 
         super(BraTS2021, self).__init__(root, modalities=modalities, learn_mode=learn_mode, extract_slice=extract_slice, 
                                         noise_type=noise_type, transform_data=transform_data, client_weights=client_weights,
                                         data_mode=data_mode, data_num=data_num, data_paired_weight=data_paired_weight,
                                         data_moda_ratio=data_moda_ratio, data_moda_case=data_moda_case, 
-                                        assigned_data=assigned_data, assigned_images=assigned_images)
+                                        assigned_data=assigned_data, assigned_images=assigned_images, seed=seed)
 
