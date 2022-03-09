665c8b32cd6f88ad3ed395aebd53591f47391a71
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-03-09 10:21:56 +0800

    fix the bugs

--- kaid.py
+++ kaid.py
@@ -189,9 +189,9 @@ if __name__ == '__main__':
     optimizer = torch.optim.Adam(kaid_ae.parameters(), lr=para_dict['lr'],
                                  betas=[para_dict['beta1'], para_dict['beta2']])
 
-    ## Scheduler
-    #lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=para_dict['step_size'],
-    #                                               gamma=para_dict['gamma']) 
+    # Scheduler
+    lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=para_dict['step_size'],
+                                                   gamma=para_dict['gamma']) 
     
     #if para_dict['dataset'] == 'ixi':
     #    """
