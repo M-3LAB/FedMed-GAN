c34b1945ac46fbeeb130379d4c22e33e63e48858
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-03-11 10:05:51 +0800

    update

--- loss_function/triplet_loss.py
+++ loss_function/triplet_loss.py
@@ -1,8 +1,8 @@
 import torch
 import torch.nn as nn
-from loss_function.distance import cosine_similiarity
+from loss_function.distance import cosine_similiarity, l1_diff, l2_diff, euclidean_distance
 
-__all__ = ['triplet_loss']
+__all__ = ['contrastive_loss']
 
-def triplet_loss():
+def contrastive_loss():
     pass
