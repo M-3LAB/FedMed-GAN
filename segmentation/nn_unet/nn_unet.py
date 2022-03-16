dd4600c0c3e041413faccf27b6d9ed88c3e5c884
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-03-16 21:47:31 +0800

    update

--- segmentation/nn_unet/nn_unet.py
+++ segmentation/nn_unet/nn_unet.py
@@ -2,3 +2,10 @@ import torch
 import torch.nn as nn
 
 __all__ = ['ExNNUnet']
+
+class ExNNUnet(nn.Module):
+    def __init__(self):
+        super(ExNNUnet).__init__()
+    
+    def forward(self, x):
+        pass
