81dcb94ebc907673b528e6a6cb6b9fb5e37c0da6
guoyang-xie <xiegy2019@mail.sustech.edu.cn>
2022-03-17 20:23:17 +0800

    update

--- segmentation/2d/extended_unet.py
+++ segmentation/2d/extended_unet.py
@@ -3,9 +3,9 @@ import torch.nn as nn
 
 __all__ = ['ExNNUnet']
 
-class ExNNUnet(nn.Module):
+class Ex2DUnet(nn.Module):
     def __init__(self):
-        super(ExNNUnet).__init__()
+        super(Ex2DUnet).__init__()
     
     def forward(self, x):
         pass
